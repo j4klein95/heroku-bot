a5d62ad002380e7c8d7c46a4bbf88d13293f696b
Jared Klein <j4klein95@gmail.com>
2017-10-31 14:06:16 -0400

    "."

--- ChatterBot.py
+++ ChatterBot.py
@@ -1,19 +1,54 @@
-# Dependencies
 import tweepy
-import json
 import time
+import json
+import random
+import requests as req
+import datetime
+import config
+
+
+
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+access_token = config.access_token
+access_token_secret = config.access_token_secret
 
-# Setup Tweepy API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+# Weather API
+api_key = config.api_key
 
 
-# Create a function that tweets
-# CODE GOES HERE
+# Create a function that gets the weather in London and Tweets it
+def WeatherTweet():
+
+    # Construct a Query URL for the OpenWeatherMap
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = "London"
+    units = "imperial"
+    query_url = url + "appid=" + api_key + "&q=" + city + "&units=" + units
+
+    # Perform the API call to get the weather
+    weather_response = req.get(query_url)
+    weather_json = weather_response.json()
+    print(weather_json)
+
+    # Twitter credentials
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    # Tweet the weather
+    api.update_status(
+        "London Weather as of %s: %s F" %
+        (datetime.datetime.now().strftime("%I:%M %p"),
+         weather_json["main"]["temp"]))
+
+    # Print success message
+    print("Tweeted successfully, sir!")
+
+
+# Set timer to run every 1 hour
+while(True):
+    WeatherTweet()
+    time.sleep(3600)
---
+++ config.py
@@ -0,0 +1,5 @@
+api_key = '019d3ce270b6a051b0a058b9843b4e7e'
+consumer_key = 'ZyCWQFJxEZzvLZOH43VgKa4V9'
+access_token = '824474730501701633-RcwiZedaSLn3a7AZx0dMNTYnFbaPgEY'
+access_token_secret = 'fDK407KAMyLBELtqqnli7MQaYvqY7NuufVpdnwNV5IQGo'
+consumer_secret = "AbhuEOmXcYfI3L1Rl4QvY0vorTeGUiAe2oYOReP38SFTnUdrB5"
