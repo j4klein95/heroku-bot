dcfed2dfae8185d6b2b4911b3c8e1164ebb214a7
Jared Klein <j4klein95@gmail.com>
2017-10-31 15:18:00 -0400

    .

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,16 +4,16 @@ import json
 import random
 import requests as req
 import datetime
-import config
+#import config
 
 #config = "./config.py"
 
 
 # Twitter API Keys
-consumer_key = config.consumer_key
-consumer_secret = config.consumer_secret
-access_token = config.access_token
-access_token_secret = config.access_token_secret
+consumer_key = #config.consumer_key
+consumer_secret = #config.consumer_secret
+access_token = #config.access_token
+access_token_secret = #config.access_token_secret
 
 # Weather API
 api_key = config.api_key
